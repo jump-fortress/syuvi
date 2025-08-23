063696ec944648727b532ab9950665434ced19ff
bailey <spiritov_v@pm.me>
2025-08-22 23:05:32 -0700

    add default divisions

--- src/events/member-join.js
+++ src/events/member-join.js
@@ -1,8 +1,8 @@
 import { divisionRoleIds } from "../lib/guild-ids.js";
-import { getPlayer } from "../lib/database.js";
+import { getPlayer, createPlayer, updatePlayerDivision } from "../lib/database.js";
 
 async function memberJoin(member) {
-  const player = getPlayer(member.id);
+  let player = getPlayer(member.id);
   if (player) {
     const roles = member.guild.roles.cache;
     if (player.soldier_division) {
@@ -13,6 +13,24 @@ async function memberJoin(member) {
       const demoRole = roles.get(divisionRoleIds.get(`${player.demo_division} Demo`));
       await member.roles.add(demoRole);
     }
+  } else {
+    // TODO: temporary measure when expecting an influx of members
+    player = createPlayer(member.id, member.displayName);
+
+    let division = {
+      class: "Soldier",
+      name: "Wood",
+    };
+    updatePlayerDivision(member.id, division);
+    division.class = "Demo";
+    updatePlayerDivision(member.id, division);
+
+    const roles = member.guild.roles.cache;
+
+    const soldierRole = roles.get(divisionRoleIds.get(`Wood Soldier`));
+    await member.roles.add(soldierRole);
+    const demoRole = roles.get(divisionRoleIds.get(`Wood Demo`));
+    await member.roles.add(demoRole);
   }
 }
 
